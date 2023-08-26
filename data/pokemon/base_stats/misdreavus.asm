3f84648af3d4fac7d8cb0ff920b1ef02dc85dc29
KRverde <114873401+KRverde@users.noreply.github.com>
2023-08-25 23:34:54 -0400

    Update misdreavus.asm

--- data/pokemon/base_stats/misdreavus.asm
+++ data/pokemon/base_stats/misdreavus.asm
@@ -1,21 +1,21 @@
-	db MISDREAVUS ; 200
+	db GASSPING ; 200
 
-	db  60,  60,  60,  85,  85,  85
+	db  65,  90, 120,  60,  85,  70
 	;   hp  atk  def  spd  sat  sdf
 
-	db GHOST, GHOST ; type
-	db 45 ; catch rate
-	db 147 ; base exp
-	db NO_ITEM, SPELL_TAG ; items
+	db POISON, POISON ; type
+	db 60 ; catch rate
+	db 173 ; base exp
+	db NO_ITEM, NO_ITEM ; items
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
-	db 25 ; step cycles to hatch
+	db 20 ; step cycles to hatch
 	db 5 ; unknown 2
-	INCBIN "gfx/pokemon/misdreavus/front.dimensions"
+	INCBIN "gfx/pokemon/gassping/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
-	db GROWTH_FAST ; growth rate
+	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_INDETERMINATE, EGG_INDETERMINATE ; egg groups
 
 	; tm/hm learnset
-	tmhm HEADBUTT, CURSE, TOXIC, ZAP_CANNON, PSYCH_UP, HIDDEN_POWER, SUNNY_DAY, SNORE, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, THUNDER, RETURN, PSYCHIC_M, SHADOW_BALL, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SWIFT, DEFENSE_CURL, DREAM_EATER, REST, ATTRACT, THIEF, NIGHTMARE, FLASH, THUNDERBOLT
+	tmhm CURSE, ROLLOUT, TOXIC, ZAP_CANNON, HIDDEN_POWER, SUNNY_DAY, SNORE, HYPER_BEAM, PROTECT, ENDURE, FRUSTRATION, THUNDER, RETURN, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SLUDGE_BOMB, FIRE_BLAST, REST, ATTRACT, THIEF, FLAMETHROWER, THUNDERBOLT
 	; end
