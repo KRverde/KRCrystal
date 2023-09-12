6e000a0f88abb9ba4943e77c32eb5b80e1cdeefb
KRverde <114873401+KRverde@users.noreply.github.com>
2023-09-11 22:23:13 -0400

    Update misdreavus.asm

--- data/pokemon/base_stats/misdreavus.asm
+++ data/pokemon/base_stats/misdreavus.asm
@@ -11,7 +11,7 @@
 	db 100 ; unknown 1
 	db 20 ; step cycles to hatch
 	db 5 ; unknown 2
-	INCBIN "gfx/pokemon/gassping/front.dimensions"
+	INCBIN "gfx/pokemon/misdreavus/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_INDETERMINATE, EGG_INDETERMINATE ; egg groups
