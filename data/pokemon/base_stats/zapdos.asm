d1c17e84fc0d6a447a93acdf9541d52d52efeb61
KRverde <114873401+KRverde@users.noreply.github.com>
2023-09-11 22:19:11 -0400

    Update zapdos.asm

--- data/pokemon/base_stats/zapdos.asm
+++ data/pokemon/base_stats/zapdos.asm
@@ -1,21 +1,21 @@
-	db ZAPDOS ; 145
+	db ELECTROLITE ; 145
 
-	db  90,  90,  85, 100, 125,  90
+	db  60,  70,  70, 160, 100,  80
 	;   hp  atk  def  spd  sat  sdf
 
-	db ELECTRIC, FLYING ; type
-	db 3 ; catch rate
-	db 216 ; base exp
+	db ELECTRIC, ELECTRIC ; type
+	db 60 ; catch rate
+	db 150 ; base exp
 	db NO_ITEM, NO_ITEM ; items
 	db GENDER_UNKNOWN ; gender ratio
 	db 100 ; unknown 1
-	db 80 ; step cycles to hatch
+	db 20 ; step cycles to hatch
 	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/zapdos/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
-	db GROWTH_SLOW ; growth rate
-	dn EGG_NONE, EGG_NONE ; egg groups
+	db GROWTH_MEDIUM_FAST ; growth rate
+	dn EGG_MINERAL, EGG_MINERAL ; egg groups
 
 	; tm/hm learnset
-	tmhm CURSE, ROAR, TOXIC, ZAP_CANNON, ROCK_SMASH, HIDDEN_POWER, SUNNY_DAY, SNORE, HYPER_BEAM, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, THUNDER, RETURN, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SANDSTORM, SWIFT, DETECT, REST, STEEL_WING, FLY, FLASH, THUNDERBOLT
+	tmhm HEADBUTT, CURSE, ROLLOUT, TOXIC, ZAP_CANNON, HIDDEN_POWER, SNORE, HYPER_BEAM, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, THUNDER, RETURN, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SWIFT, REST, FLASH, THUNDERBOLT
 	; end
