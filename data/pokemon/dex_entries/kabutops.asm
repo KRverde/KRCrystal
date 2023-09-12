73d68017968fc200f619e708b464638836ca0d8d
KRverde <114873401+KRverde@users.noreply.github.com>
2023-09-11 22:40:01 -0400

    Update kabutops.asm

--- data/pokemon/dex_entries/kabutops.asm
+++ data/pokemon/dex_entries/kabutops.asm
@@ -1,10 +1,10 @@
 	db "SHELLFISH@" ; species name
 	dw 403, 890 ; height, weight
 
-	db   "It was able to"
-	next "swim quickly thro-"
-	next "ugh the water by"
+	db   "Its body is adept"
+	next "to swimming even"
+	next "against rough ocean"
 
-	page "compactly folding"
-	next "up its razor-sharp"
-	next "sickles.@"
+	page "currents. It's"
+	next "much more bug-like"
+	next "than its ancestors.@"
