a1b66713c596030de30beb6fe80c269564a5350d
KRverde <114873401+KRverde@users.noreply.github.com>
2023-09-11 22:34:56 -0400

    Update omanyte.asm

--- data/pokemon/dex_entries/omanyte.asm
+++ data/pokemon/dex_entries/omanyte.asm
@@ -1,10 +1,10 @@
 	db "SPIRAL@" ; species name
 	dw 104, 170 ; height, weight
 
-	db   "In prehistoric"
-	next "times, it swam on"
-	next "the sea floor,"
+	db   "New predators"
+	next "caused its shell"
+	next "to harden to"
 
-	page "eating plankton."
-	next "Its fossils are"
-	next "sometimes found.@"
+	page "steel-like levels."
+	next "It eats plankton"
+	next "off the seafloor.@"
