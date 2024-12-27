bda48dfb5d58c27aeb72347d9952705ba9a3c0af
KRverde <114873401+KRverde@users.noreply.github.com>
2024-12-27 14:53:24 -0500

    Update kangaskhan.asm

--- data/pokemon/base_stats/kangaskhan.asm
+++ data/pokemon/base_stats/kangaskhan.asm
@@ -1,21 +1,21 @@
-	db KANGASKHAN ; 115
+	db CLENCHIR ; 115
 
-	db 105,  95,  80,  90,  40,  80
+	db  75, 135, 100,  45,  55,  90
 	;   hp  atk  def  spd  sat  sdf
 
-	db NORMAL, NORMAL ; type
+	db BUG, FLYING ; type
 	db 45 ; catch rate
-	db 175 ; base exp
+	db 200 ; base exp
 	db NO_ITEM, NO_ITEM ; items
-	db GENDER_F100 ; gender ratio
+	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
-	db 20 ; step cycles to hatch
+	db 25 ; step cycles to hatch
 	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/kangaskhan/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
-	db GROWTH_MEDIUM_FAST ; growth rate
-	dn EGG_MONSTER, EGG_MONSTER ; egg groups
+	db GROWTH_SLOW ; growth rate
+	dn EGG_BUG, EGG_BUG ; egg groups
 
 	; tm/hm learnset
-	tmhm DYNAMICPUNCH, HEADBUTT, CURSE, ROAR, TOXIC, ZAP_CANNON, ROCK_SMASH, HIDDEN_POWER, SUNNY_DAY, SNORE, BLIZZARD, HYPER_BEAM, ICY_WIND, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, IRON_TAIL, THUNDER, EARTHQUAKE, RETURN, SHADOW_BALL, MUD_SLAP, DOUBLE_TEAM, ICE_PUNCH, SWAGGER, SLEEP_TALK, SANDSTORM, FIRE_BLAST, THUNDERPUNCH, REST, ATTRACT, FIRE_PUNCH, FURY_CUTTER, SURF, STRENGTH, FLAMETHROWER, THUNDERBOLT, ICE_BEAM
+	tmhm HEADBUTT, CURSE, TOXIC, ROCK_SMASH, HIDDEN_POWER, SUNNY_DAY, SNORE, HYPER_BEAM, PROTECT, ENDURE, FRUSTRATION, RETURN, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, REST, ATTRACT, THIEF, FURY_CUTTER, CUT, FLY, STRENGTH
 	; end
