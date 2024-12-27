938b382d4db4dfd2b6b872fb2ad8349c0d31d8b1
KRverde <114873401+KRverde@users.noreply.github.com>
2024-12-27 14:26:03 -0500

    Update dodrio.asm

--- data/pokemon/base_stats/dodrio.asm
+++ data/pokemon/base_stats/dodrio.asm
@@ -1,12 +1,12 @@
-	db DODRIO ; 085
+	db URSALUNA ; 085
 
-	db  60, 110,  70, 100,  60,  60
+	db 130, 140, 105,  50,  45,  80
 	;   hp  atk  def  spd  sat  sdf
 
-	db NORMAL, FLYING ; type
-	db 45 ; catch rate
-	db 158 ; base exp
-	db NO_ITEM, SHARP_BEAK ; items
+	db NORMAL, GROUND ; type
+	db 60 ; catch rate
+	db 189 ; base exp
+	db NO_ITEM, NO_ITEM ; items
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 20 ; step cycles to hatch
@@ -14,8 +14,8 @@
 	INCBIN "gfx/pokemon/dodrio/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_MEDIUM_FAST ; growth rate
-	dn EGG_FLYING, EGG_FLYING ; egg groups
+	dn EGG_GROUND, EGG_GROUND ; egg groups
 
 	; tm/hm learnset
-	tmhm CURSE, TOXIC, HIDDEN_POWER, SUNNY_DAY, SNORE, HYPER_BEAM, PROTECT, ENDURE, FRUSTRATION, RETURN, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SWIFT, REST, ATTRACT, THIEF, STEEL_WING, FLY
+	tmhm DYNAMICPUNCH, HEADBUTT, CURSE, ROLLOUT, ROAR, TOXIC, ZAP_CANNON, ROCK_SMASH, HIDDEN_POWER, SUNNY_DAY, SNORE, HYPER_BEAM, PROTECT, ENDURE, FRUSTRATION, EARTHQUAKE, RETURN, DIG, MUD_SLAP, DOUBLE_TEAM, ICE_PUNCH, SWAGGER, SLEEP_TALK, SWIFT, DEFENSE_CURL, THUNDERPUNCH, REST, ATTRACT, THIEF, FIRE_PUNCH, FURY_CUTTER, CUT, STRENGTH
 	; end
