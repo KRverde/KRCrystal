2e797627142025fe0356a6f2ef5c17555aaf888c
KRverde <114873401+KRverde@users.noreply.github.com>
2024-12-26 20:42:13 -0500

    Update ivysaur.asm

--- data/pokemon/base_stats/ivysaur.asm
+++ data/pokemon/base_stats/ivysaur.asm
@@ -1,21 +1,21 @@
-	db IVYSAUR ; 002
+	db GLACEON ; 002
 
 	db  60,  62,  63,  60,  80,  80
 	;   hp  atk  def  spd  sat  sdf
 
-	db GRASS, POISON ; type
+	db ICE, ICE ; type
 	db 45 ; catch rate
-	db 141 ; base exp
+	db 197 ; base exp
 	db NO_ITEM, NO_ITEM ; items
 	db GENDER_F12_5 ; gender ratio
 	db 100 ; unknown 1
-	db 20 ; step cycles to hatch
+	db 35 ; step cycles to hatch
 	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/ivysaur/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
-	db GROWTH_MEDIUM_SLOW ; growth rate
-	dn EGG_MONSTER, EGG_PLANT ; egg groups
+	db GROWTH_MEDIUM_FAST ; growth rate
+	dn EGG_GROUND, EGG_GROUND ; egg groups
 
 	; tm/hm learnset
-	tmhm HEADBUTT, CURSE, TOXIC, HIDDEN_POWER, SUNNY_DAY, SWEET_SCENT, SNORE, PROTECT, GIGA_DRAIN, ENDURE, FRUSTRATION, SOLARBEAM, RETURN, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, DEFENSE_CURL, REST, ATTRACT, FURY_CUTTER, CUT, FLASH
+	tmhm HEADBUTT, CURSE, TOXIC, ROCK_SMASH, HIDDEN_POWER, SUNNY_DAY, SNORE, BLIZZARD, HYPER_BEAM, ICY_WIND, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, IRON_TAIL, RETURN, SHADOW_BALL, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SWIFT, DETECT, REST, ATTRACT, STRENGTH
 	; end
