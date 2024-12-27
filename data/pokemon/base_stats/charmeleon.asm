d412ea58144ed4dbf81f52905cd35bb497fb4af8
KRverde <114873401+KRverde@users.noreply.github.com>
2024-12-26 22:17:32 -0500

    Update charmeleon.asm

--- data/pokemon/base_stats/charmeleon.asm
+++ data/pokemon/base_stats/charmeleon.asm
@@ -1,21 +1,21 @@
-	db CHARMELEON ; 005
+	db KLEAVOR ; 005
 
-	db  58,  64,  58,  80,  80,  65
+	db  70, 135,  95,  85,  45,  70
 	;   hp  atk  def  spd  sat  sdf
 
-	db FIRE, FIRE ; type
+	db BUG, ROCK ; type
 	db 45 ; catch rate
-	db 142 ; base exp
+	db 187 ; base exp
 	db NO_ITEM, NO_ITEM ; items
-	db GENDER_F12_5 ; gender ratio
+	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
-	db 20 ; step cycles to hatch
+	db 25 ; step cycles to hatch
 	db 5 ; unknown 2
-	INCBIN "gfx/pokemon/charmeleon/front.dimensions"
+	INCBIN "gfx/pokemon/scyther/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
-	db GROWTH_MEDIUM_SLOW ; growth rate
-	dn EGG_MONSTER, EGG_DRAGON ; egg groups
+	db GROWTH_MEDIUM_FAST ; growth rate
+	dn EGG_BUG, EGG_BUG ; egg groups
 
 	; tm/hm learnset
-	tmhm DYNAMICPUNCH, HEADBUTT, CURSE, TOXIC, ROCK_SMASH, HIDDEN_POWER, SUNNY_DAY, SNORE, PROTECT, ENDURE, FRUSTRATION, IRON_TAIL, DRAGONBREATH, RETURN, DIG, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, FIRE_BLAST, SWIFT, DEFENSE_CURL, REST, ATTRACT, FIRE_PUNCH, FURY_CUTTER, CUT, STRENGTH, FLAMETHROWER
+	tmhm HEADBUTT, CURSE, TOXIC, ROCK_SMASH, HIDDEN_POWER, SUNNY_DAY, SNORE, HYPER_BEAM, PROTECT, ENDURE, FRUSTRATION, RETURN, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SANDSTORM, SWIFT, DETECT, REST, ATTRACT, THIEF, STEEL_WING, FURY_CUTTER, CUT
 	; end
