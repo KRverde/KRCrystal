827339ac536aeb8fd4cf223f5aed4a79c12c0431
KRverde <114873401+KRverde@users.noreply.github.com>
2024-12-27 14:37:14 -0500

    Update rapidash.asm

--- data/pokemon/base_stats/rapidash.asm
+++ data/pokemon/base_stats/rapidash.asm
@@ -1,12 +1,12 @@
-	db RAPIDASH ; 078
+	db PERISHROOM ; 078
 
-	db  65, 100,  70, 105,  80,  80
+	db  85, 125, 100,  40,  65, 100
 	;   hp  atk  def  spd  sat  sdf
 
-	db FIRE, FIRE ; type
-	db 60 ; catch rate
-	db 192 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	db BUG, GHOST ; type
+	db 75 ; catch rate
+	db 178 ; base exp
+	db TINYMUSHROOM, BIG_MUSHROOM ; items
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 20 ; step cycles to hatch
@@ -14,8 +14,8 @@
 	INCBIN "gfx/pokemon/rapidash/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_MEDIUM_FAST ; growth rate
-	dn EGG_GROUND, EGG_GROUND ; egg groups
+	dn EGG_BUG, EGG_PLANT ; egg groups
 
 	; tm/hm learnset
-	tmhm HEADBUTT, CURSE, TOXIC, HIDDEN_POWER, SUNNY_DAY, SNORE, HYPER_BEAM, PROTECT, ENDURE, FRUSTRATION, IRON_TAIL, RETURN, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, FIRE_BLAST, SWIFT, REST, ATTRACT, FLAMETHROWER
+	tmhm CURSE, TOXIC, ROCK_SMASH, HIDDEN_POWER, SUNNY_DAY, SWEET_SCENT, SNORE, HYPER_BEAM, PROTECT, GIGA_DRAIN, ENDURE, FRUSTRATION, SOLARBEAM, RETURN, DIG, SHADOW_BALL, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SLUDGE_BOMB, REST, ATTRACT, THIEF, FURY_CUTTER, NIGHTMARE, CUT, FLASH
 	; end
