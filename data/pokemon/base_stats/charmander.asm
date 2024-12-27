7c5e3b5b6c88811145d32899b1e7482463b97678
KRverde <114873401+KRverde@users.noreply.github.com>
2024-12-26 20:48:02 -0500

    Update charmander.asm

--- data/pokemon/base_stats/charmander.asm
+++ data/pokemon/base_stats/charmander.asm
@@ -1,21 +1,21 @@
-	db CHARMANDER ; 004
+	db SYLVEON ; 004
 
-	db  39,  52,  43,  65,  60,  50
+	db  95,  65,  65,  60, 110, 130
 	;   hp  atk  def  spd  sat  sdf
 
-	db FIRE, FIRE ; type
+	db NORMAL, NORMAL ; type
 	db 45 ; catch rate
-	db 65 ; base exp
+	db 92 ; base exp
 	db NO_ITEM, NO_ITEM ; items
 	db GENDER_F12_5 ; gender ratio
 	db 100 ; unknown 1
-	db 20 ; step cycles to hatch
+	db 35 ; step cycles to hatch
 	db 5 ; unknown 2
-	INCBIN "gfx/pokemon/charmander/front.dimensions"
+	INCBIN "gfx/pokemon/eevee/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
-	db GROWTH_MEDIUM_SLOW ; growth rate
-	dn EGG_MONSTER, EGG_DRAGON ; egg groups
+	db GROWTH_MEDIUM_FAST ; growth rate
+	dn EGG_GROUND, EGG_GROUND ; egg groups
 
 	; tm/hm learnset
-	tmhm DYNAMICPUNCH, HEADBUTT, CURSE, TOXIC, ROCK_SMASH, HIDDEN_POWER, SUNNY_DAY, SNORE, PROTECT, ENDURE, FRUSTRATION, IRON_TAIL, DRAGONBREATH, RETURN, DIG, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, FIRE_BLAST, SWIFT, DEFENSE_CURL, REST, ATTRACT, FIRE_PUNCH, FURY_CUTTER, CUT, STRENGTH, FLAMETHROWER
+	tmhm HEADBUTT, CURSE, TOXIC, PSYCH_UP, HIDDEN_POWER, SUNNY_DAY, SNORE, HYPER_BEAM, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, IRON_TAIL, RETURN, SHADOW_BALL, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SWIFT, DETECT, REST, ATTRACT, CUT, FLASH
 	; end
