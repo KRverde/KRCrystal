ee56050df79156ec4d8a478ee43f75ccf2fa8d3f
KRverde <114873401+KRverde@users.noreply.github.com>
2024-12-26 22:21:17 -0500

    Update squirtle.asm

--- data/pokemon/base_stats/squirtle.asm
+++ data/pokemon/base_stats/squirtle.asm
@@ -1,21 +1,21 @@
-	db SQUIRTLE ; 007
+	db MAGNEZONE ; 007
 
-	db  44,  48,  65,  43,  50,  64
+	db  70,  70, 115,  60, 130,  90
 	;   hp  atk  def  spd  sat  sdf
 
-	db WATER, WATER ; type
-	db 45 ; catch rate
-	db 66 ; base exp
-	db NO_ITEM, NO_ITEM ; items
-	db GENDER_F12_5 ; gender ratio
+	db ELECTRIC, STEEL ; type
+	db 60 ; catch rate
+	db 161 ; base exp
+	db NO_ITEM, METAL_COAT ; items
+	db GENDER_UNKNOWN ; gender ratio
 	db 100 ; unknown 1
 	db 20 ; step cycles to hatch
 	db 5 ; unknown 2
-	INCBIN "gfx/pokemon/squirtle/front.dimensions"
+	INCBIN "gfx/pokemon/magneton/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
-	db GROWTH_MEDIUM_SLOW ; growth rate
-	dn EGG_MONSTER, EGG_WATER_1 ; egg groups
+	db GROWTH_MEDIUM_FAST ; growth rate
+	dn EGG_MINERAL, EGG_MINERAL ; egg groups
 
 	; tm/hm learnset
-	tmhm DYNAMICPUNCH, HEADBUTT, CURSE, ROLLOUT, TOXIC, ROCK_SMASH, HIDDEN_POWER, SNORE, BLIZZARD, ICY_WIND, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, IRON_TAIL, RETURN, DIG, MUD_SLAP, DOUBLE_TEAM, ICE_PUNCH, SWAGGER, SLEEP_TALK, DEFENSE_CURL, REST, ATTRACT, SURF, STRENGTH, WHIRLPOOL, WATERFALL, ICE_BEAM
+	tmhm CURSE, ROLLOUT, TOXIC, ZAP_CANNON, HIDDEN_POWER, SNORE, HYPER_BEAM, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, THUNDER, RETURN, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SWIFT, REST, FLASH, THUNDERBOLT
 	; end
