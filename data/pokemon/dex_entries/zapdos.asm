5537990ba0788228fb55a913e9c913cb42877fdc
KRverde <114873401+KRverde@users.noreply.github.com>
2024-12-27 17:50:55 -0500

    Update zapdos.asm

--- data/pokemon/dex_entries/zapdos.asm
+++ data/pokemon/dex_entries/zapdos.asm
@@ -1,8 +1,8 @@
 	db "BALL@" ; species name
 	dw 411, 2068 ; height, weight
 
-	db   "Unparalled in speed,"
-	next "It is said to"
+	db   "Unparalled in spe-"
+	next "ed, it is said to"
 	next "originate from"
 
 	page "ELECTRODE that are"
