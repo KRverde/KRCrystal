8ca1b13220e592b6f2a20d44cd8b2837184b4c49
KRverde <114873401+KRverde@users.noreply.github.com>
2024-12-26 22:35:00 -0500

    Update primeape.asm

--- data/pokemon/base_stats/primeape.asm
+++ data/pokemon/base_stats/primeape.asm
@@ -1,21 +1,21 @@
-	db PRIMEAPE ; 057
+	db MAGMORTAR ; 057
 
-	db  65, 105,  60,  95,  60,  70
+	db  75,  95,  67,  83, 125,  95
 	;   hp  atk  def  spd  sat  sdf
 
-	db FIGHTING, FIGHTING ; type
-	db 75 ; catch rate
-	db 149 ; base exp
-	db NO_ITEM, NO_ITEM ; items
-	db GENDER_F50 ; gender ratio
+	db FIRE, FIRE ; type
+	db 45 ; catch rate
+	db 167 ; base exp
+	db BURNT_BERRY, BURNT_BERRY ; items
+	db GENDER_F25 ; gender ratio
 	db 100 ; unknown 1
-	db 20 ; step cycles to hatch
+	db 25 ; step cycles to hatch
 	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/primeape/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_MEDIUM_FAST ; growth rate
-	dn EGG_GROUND, EGG_GROUND ; egg groups
+	dn EGG_HUMANSHAPE, EGG_HUMANSHAPE ; egg groups
 
 	; tm/hm learnset
-	tmhm DYNAMICPUNCH, HEADBUTT, CURSE, TOXIC, ROCK_SMASH, PSYCH_UP, HIDDEN_POWER, SUNNY_DAY, SNORE, HYPER_BEAM, PROTECT, ENDURE, FRUSTRATION, IRON_TAIL, THUNDER, RETURN, DIG, MUD_SLAP, DOUBLE_TEAM, ICE_PUNCH, SWAGGER, SLEEP_TALK, SWIFT, DEFENSE_CURL, THUNDERPUNCH, DETECT, REST, ATTRACT, THIEF, FIRE_PUNCH, STRENGTH, THUNDERBOLT
+	tmhm DYNAMICPUNCH, HEADBUTT, CURSE, TOXIC, ROCK_SMASH, HIDDEN_POWER, SUNNY_DAY, SNORE, HYPER_BEAM, PROTECT, ENDURE, FRUSTRATION, IRON_TAIL, RETURN, PSYCHIC_M, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, FIRE_BLAST, THUNDERPUNCH, DETECT, REST, ATTRACT, THIEF, FIRE_PUNCH, STRENGTH, FLAMETHROWER
 	; end
