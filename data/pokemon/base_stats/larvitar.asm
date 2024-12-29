3db82843f7b87f1204757f2a0b340e94186bd361
KRverde <114873401+KRverde@users.noreply.github.com>
2024-12-28 20:02:09 -0500

    Update larvitar.asm

--- data/pokemon/base_stats/larvitar.asm
+++ data/pokemon/base_stats/larvitar.asm
@@ -1,21 +1,21 @@
-	db LARVITAR ; 246
+	db KELPIQUUS ; 246
 
-	db  50,  64,  50,  41,  45,  50
+	db  75,  75,  95,  95, 125,  75
 	;   hp  atk  def  spd  sat  sdf
 
-	db ROCK, GROUND ; type
-	db 45 ; catch rate
-	db 67 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	db WATER, DARK ; type
+	db 75 ; catch rate
+	db 155 ; base exp
+	db NO_ITEM, DRAGON_SCALE ; items
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
-	db 40 ; step cycles to hatch
+	db 20 ; step cycles to hatch
 	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/larvitar/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
-	db GROWTH_SLOW ; growth rate
-	dn EGG_MONSTER, EGG_MONSTER ; egg groups
+	db GROWTH_MEDIUM_FAST ; growth rate
+	dn EGG_WATER_1, EGG_DRAGON ; egg groups
 
 	; tm/hm learnset
-	tmhm HEADBUTT, CURSE, TOXIC, HIDDEN_POWER, SUNNY_DAY, SNORE, HYPER_BEAM, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, EARTHQUAKE, RETURN, DIG, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SANDSTORM, DETECT, REST, ATTRACT
+	tmhm HEADBUTT, CURSE, TOXIC, HIDDEN_POWER, SNORE, BLIZZARD, HYPER_BEAM, ICY_WIND, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, DRAGONBREATH, RETURN, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SWIFT, REST, ATTRACT, SURF, WHIRLPOOL, WATERFALL, ICE_BEAM
 	; end
