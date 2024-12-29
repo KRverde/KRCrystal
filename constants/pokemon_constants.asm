f06c260d3069be98058ebe3a88db8a9f5569f9a8
KRverde <114873401+KRverde@users.noreply.github.com>
2024-12-28 20:04:58 -0500

    Update pokemon_constants.asm

--- constants/pokemon_constants.asm
+++ constants/pokemon_constants.asm
@@ -265,7 +265,7 @@ DEF JOHTO_POKEMON EQU const_value
 	const RAIKOU     ; f3
 	const ENTEI      ; f4
 	const SUICUNE    ; f5
-	const LARVITAR   ; f6
+	const KELPIQUUS  ; f6
 	const TROPIUS    ; f7
 	const TROPIPOD   ; f8
 	const LUGIA      ; f9
