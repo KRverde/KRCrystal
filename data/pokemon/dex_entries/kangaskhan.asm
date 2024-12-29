ff0802507a7b80a429ce151740f765bad24ff23c
KRverde <114873401+KRverde@users.noreply.github.com>
2024-12-28 19:47:50 -0500

    Update kangaskhan.asm

--- data/pokemon/dex_entries/kangaskhan.asm
+++ data/pokemon/dex_entries/kangaskhan.asm
@@ -1,5 +1,5 @@
-	db "STAG BEETLE@" ; species name
-	dw 511, 1510 ; height, weight
+	db "STAGBEETLE@" ; species name
+	dw 511, 1710 ; height, weight
 
 	db   "It has sprouted"
 	next "wings which allow"
