df216fc473dd5423d7e62dae0ff8ba520cd579d4
KRverde <114873401+KRverde@users.noreply.github.com>
2024-12-30 13:29:04 -0500

    Update cries.asm

--- data/pokemon/cries.asm
+++ data/pokemon/cries.asm
@@ -61,7 +61,7 @@ PokemonCries::
 	mon_cry CRY_CLEFAIRY,    153,  383 ; PERSIAN
 	mon_cry CRY_PSYDUCK,      32,  224 ; PSYDUCK
 	mon_cry CRY_PSYDUCK,     255,  192 ; GOLDUCK
-	mon_cry CRY_NIDOQUEEN,   221,  224 ; MANKEY
+	mon_cry CRY_VOLTORB,      43,  383 ; ELECTIVIRE
 	mon_cry CRY_NIDOQUEEN,   175,  192 ; PRIMEAPE
 	mon_cry CRY_GROWLITHE,    32,  192 ; GROWLITHE
 	mon_cry CRY_WEEDLE,        0,  256 ; ARCANINE
