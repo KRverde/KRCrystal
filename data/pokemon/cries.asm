9fa9552684d07e6069e10d6798c4d8096a34072c
KRverde <114873401+KRverde@users.noreply.github.com>
2024-12-30 19:57:00 -0500

    Update cries.asm

--- data/pokemon/cries.asm
+++ data/pokemon/cries.asm
@@ -184,7 +184,7 @@ PokemonCries::
 	mon_cry CRY_TOGETIC,      59,   56 ; TOGETIC
 	mon_cry CRY_NATU,       -103,  256 ; NATU
 	mon_cry CRY_NATU,       -167,  360 ; XATU
-	mon_cry CRY_MAREEP,       34,  216 ; MAREEP
+	mon_cry CRY_GIRAFARIG,    55,  576 ; PORYGON_Z
 	mon_cry CRY_MAREEP,       -7,  384 ; FLAAFFY
 	mon_cry CRY_AMPHAROS,   -124,  232 ; AMPHAROS
 	mon_cry CRY_CLEFFA,      132,  336 ; BELLOSSOM
